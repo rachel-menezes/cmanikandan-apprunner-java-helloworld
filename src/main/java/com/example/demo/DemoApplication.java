3e9171bd8187c8ea71a218800fc5ea49ca55afec
Rachel Menezes <rachme@amazon.com>
2024-03-14 15:41:18 -0700

    Multi threading + logs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableAsync
+public class DemoApplication implements AsyncConfigurer {
 
 	public static void main(String[] args) {
+
+        final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+        log.info("Before Starting application");
 		SpringApplication.run(DemoApplication.class, args);
-	}
+        log.info("Started application");
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("ThreadNum-");
+        executor.initialize();
+        return executor;
+    }
 
 }
--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,15 +1,27 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.scheduling.annotation.Async;
 
 @RestController
 public class HelloWorld {
 
+    static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
     @GetMapping("/")
+    @Async
     public String index() {
+        log.info("------------Before returning response-------------");
+        for( int i = 0; i < 100000; i++){
+            log.info("The log line printed is for {} and thread {}", i, Thread.currentThread().getName());
+        }
         String s = "Hello World from AWS App Runner. The JDK version is " + System.getProperty("java.version");
+        log.info("------------After returning response-------------");
         return s;
+
     }
 
 }
